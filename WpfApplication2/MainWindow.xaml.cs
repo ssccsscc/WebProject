81f2ceb072e0153c249d657dd0d2387533ac92b5
ssccsscc <fccdp@mail.ru>
2018-12-01 14:54:29 +0500

    test

--- WpfApplication2/MainWindow.xaml.cs
+++ WpfApplication2/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApplication2
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
